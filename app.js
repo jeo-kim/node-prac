45745c2e0e863e45275157745cb6bd7e6f6ef437
jeonghyeon <skjh0807s@gmail.com>
2022-04-29 15:11:01 +0900

    separate routes

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
+"use strict";
+
+// 모듈
 const express = require("express");
 const app = express();
 
+const PORT = 3000;
+
+// 라우팅
+const home = require("./routes/home");
+
+// 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+app.use("/", home); //use :미들 웨어 등록하는 메소드
 
-
-app.get("/", (req, res) => {
-    res.render("home/index");
-});
-
-app.get("/login", (req, res) => {
-    res.render("home/login");
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("서버 가동")
 });
